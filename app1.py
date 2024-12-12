d844ea96419b6e0981dd719ad670c3c70fd241ad
Nathan Rossow <nathanrossow@gmail.com>
2024-12-12 00:35:35 -0600

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,4 @@
+# create a scraper for dark eden using streamlit cloud, python 3.10 and additional logic for the user to:
+# allow the user to input a url
+# allow the user to press a button to begin scrape
+# using pandas package/module, use pandas as dataframe to return the scraped data and allow the user to view and download it via .csv
